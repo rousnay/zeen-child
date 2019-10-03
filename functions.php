0aa1fbc44bb6b8abf6512428464000075f60ac9b
Md Mozahidur Rahman <mr.rousnay@gmail.com>
2019-10-03 12:10:35 +0600

    Improvment of Site styles and refactoring of codes

--- functions.php
+++ functions.php
@@ -9,11 +9,20 @@ add_action(  'wp_enqueue_scripts', 'zeen_child_enqueue_styles' );
 
 
 
-	/*
-	 * Enable support for Post Thumbnails on posts and pages.
-	 *
-	 * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
-	 */
-	add_theme_support( 'post-thumbnails' );
-	add_image_size( 'entrance-block-thumb', 1000, 600, true );
-	add_image_size( 'entrance-block-thumb-bg', 900, 900, true );
+/*
+ * Enable support for Post Thumbnails on posts and pages.
+ *
+ * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
+ */
+add_theme_support( 'post-thumbnails' );
+add_image_size( 'entrance-block-thumb', 1000, 600, true );
+add_image_size( 'entrance-block-thumb-bg', 900, 900, true );
+
+/*
+ * Enable support for Post Thumbnails on posts and pages.
+ */
+
+function ksa_custom_scripts() {
+    wp_enqueue_script( 'ksa-custom-js', get_stylesheet_directory_uri() . '/js/ksa-custom.js', array( 'jquery' ),'',true );
+}
+add_action( 'wp_enqueue_scripts', 'ksa_custom_scripts' );
--- page-home.php
+++ page-home.php
@@ -56,35 +56,6 @@ $post_wrap_class = zeen_post_wrap_class( $post->ID, $args, array( 'page-wrap' ) 
 				<?wp_reset_query(); ?>
 			</a>
 		</div>
-
-		<script type="text/javascript">
-			jQuery(".stage > .cycler > .entrance-thumb:first").addClass("active");
-			jQuery(".screen > .cycler > .entrance-thumb:first").addClass("active");
-
-			function cycleImages(){
-
-			      var $activeStage = jQuery('.stage > .cycler .active');
-			      var $nextStage = ($activeStage.next().length > 0) ? $activeStage.next() : jQuery('.stage > .cycler .entrance-thumb:first');
-			      $nextStage.css('z-index',2);//move the next image up the pile
-			      $activeStage.fadeOut(1500,function(){//fade out the top image
-				      $activeStage.css('z-index',1).show().removeClass('active');//reset the z-index and unhide the image
-			          $nextStage.css('z-index',3).addClass('active');//make the next image the top one
-			      });
-
-			      var $activeScreen = jQuery('.screen > .cycler .active');
-			      var $nextScreen = ($activeScreen.next().length > 0) ? $activeScreen.next() : jQuery('.screen > .cycler .entrance-thumb:first');
-			      $nextScreen.css('z-index',2);//move the next image up the pile
-			      $activeScreen.fadeOut(1500,function(){//fade out the top image
-				      $activeScreen.css('z-index',1).show().removeClass('active');//reset the z-index and unhide the image
-			          $nextScreen.css('z-index',3).addClass('active');//make the next image the top one
-			      });
-			    }
-
-			jQuery(document).ready(function(){
-			// run every 4s
-			setInterval('cycleImages()', 4000);
-			});
-		</script>
 	</div>
 </div><!-- .content-area -->
 
